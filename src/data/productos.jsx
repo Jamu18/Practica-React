98333fd9952eeb529eb48f8dbf341a9681fa077e
Jaime <jaimeamm18@outlook.com>
2025-07-10 19:04:51 -0500

    Mejora visual pagina principal

--- src/data/productos.jsx
+++ src/data/productos.jsx
@@ -1,38 +1,83 @@
 import imgBanner1 from '../assets/banners/banner.jpg'
-import imgCamiseta from '../assets/productos/camisetaN.jpeg'
-import imgCamisetaA from '../assets/productos/camisetaA.jpeg'
-import imgZapatosN from '../assets/productos/zapatosN.jpeg'
-import imgZapatosA from '../assets/productos/zapatosC.jpeg'
+import imgCamisetaN from '../assets/productos/camisetaN.jpeg'; // Camiseta Nike
+import imgPantalonN from '../assets/productos/pantalonN.jpg';  // Pantalón Nike
+import imgZapatosN from '../assets/productos/zapatosN.jpeg';   // Zapatos Nike
+
+import imgCamisetaA from '../assets/productos/camisetaA.jpeg'; // Camiseta Adidas
+import imgPantalonA from '../assets/productos/pantalonA.jpeg'; // Pantalón Adidas
+import imgZapatosA from '../assets/productos/ZapatosA.jpg';   // Zapatos Adidas (usa la de Nike si no tienes otra)
+
+import imgCamisetaC from '../assets/productos/camisetaC.jpg';  // Camiseta Converse
+import imgPantalonC from '../assets/productos/mochilaC.jpeg';  // Pantalón Converse (puedes cambiar por otra imagen si tienes)
+import imgZapatosC from '../assets/productos/zapatosC.jpeg';   // Zapatos Converse (usa la de Nike si no tienes otra)
+
 
 const productos = [
-
+  // Nike
   {
     id: 1,
-    nombre: "Pantalón Deportivo",
-    precio: "$40",
-    imagen: imgCamiseta,
+    nombre: "Camiseta Nike",
+    precio: "$30",
+    imagen: imgCamisetaN,
     marca: "Nike",
   },
   {
     id: 2,
-    nombre: "Camiseta Casual",
+    nombre: "Pantalón Nike",
+    precio: "$40",
+    imagen: imgPantalonN,
+    marca: "Nike",
+  },
+  {
+    id: 3,
+    nombre: "Zapatos Nike",
+    precio: "$60",
+    imagen: imgZapatosN,
+    marca: "Nike",
+  },
+  // Adidas
+  {
+    id: 4,
+    nombre: "Camiseta Adidas",
     precio: "$25",
     imagen: imgCamisetaA,
     marca: "Adidas",
   },
   {
-    id: 3,
-    nombre: "Zapatos Running",
-    precio: "$60",
-    imagen: imgZapatosN,
-    marca: "Nike",
+    id: 5,
+    nombre: "Pantalón Adidas",
+    precio: "$35",
+    imagen: imgPantalonA,
+    marca: "Adidas",
   },
   {
-    id: 4,
-    nombre: "Zapatos Clásicos",
+    id: 6,
+    nombre: "Zapatos Adidas",
     precio: "$55",
     imagen: imgZapatosA,
-    marca: "Converse  ",
+    marca: "Adidas",
+  },
+  // Converse
+  {
+    id: 7,
+    nombre: "Camiseta Converse",
+    precio: "$28",
+    imagen: imgCamisetaC,
+    marca: "Converse",
+  },
+  {
+    id: 8,
+    nombre: "Pantalón Converse",
+    precio: "$38",
+    imagen: imgPantalonC,
+    marca: "Converse",
+  },
+  {
+    id: 9,
+    nombre: "Zapatos Converse",
+    precio: "$50",
+    imagen: imgZapatosC,
+    marca: "Converse",
   },
 ];
 
